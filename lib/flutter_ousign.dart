2790cd7c73a3d33cfeb4f6c9061664227c12db3c
lizhen <lizhenshow@gmail.com>
2019-09-03 15:57:43 +0800

    The plug-in call

--- lib/flutter_ousign.dart
+++ lib/flutter_ousign.dart
@@ -10,4 +10,49 @@ class FlutterOusign {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  /// 初始化
+  static Future<String> initialize({
+    String orgID,
+    String appID,
+    bool isProd,
+  }) async {
+    final String result = await _channel.invokeMethod(
+      'initialize',
+      {
+        'orgID': orgID,
+        'appID': appID,
+        'isProd': isProd,
+      },
+    );
+    return result;
+  }
+
+  /// 请求随机数
+  static Future<String> getRandom({
+    String userName,
+    String identityTypeCode,
+    String identityNumber,
+    String phoneNumber,
+    String deviceID,
+    String userInfo,
+  }) async {
+    final String result = await _channel.invokeMethod(
+      'getRandom',
+      {
+        'userName': userName,
+        'identityTypeCode': identityTypeCode,
+        'identityNumber ': identityNumber,
+        'phoneNumber': phoneNumber,
+        'deviceID': deviceID,
+        'userInfo': userInfo,
+      },
+    );
+    return result;
+  }
+
+  /// 验证支付密码
+  static Future<String> showVerifyPassword() async {
+    await _channel.invokeMethod('showVerifyPassword');
+  }
 }
